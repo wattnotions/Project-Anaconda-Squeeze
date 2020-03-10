708c85ee168d1269507aa9848531795ab88c324e
wattnotions <shaneormonde@gmail.com>
2020-03-10 10:48:32 +0000

    fixed tab alignment issues

--- p5/Anaconda/sketch.js
+++ p5/Anaconda/sketch.js
@@ -47,76 +47,74 @@ function setup() {
 }
 
 function draw() {
-  background('white');
-  
-  
-  
-   
-  
-  
-  
-  
-  if (keyIsDown(UP_ARROW)) {
-    y -= 5;
-  }
-  if (keyIsDown(DOWN_ARROW)) {
-    y += 5;
-  }
- 
-  //This section prints the overlay stuff to screen (refresh)
-  textAlign(LEFT, LEFT);
-  fill(255,150)
-  noStroke()
-  textSize(32);
-  fill(0, 102, 153);
-  text("Current Pressure Level :" +y, 10, 30);
-  
- 
-  
- 
-  //This part draws the line that has persistence between frames
-  canvas2.stroke(5);
-  canvas2.fill(100);
-  canvas2. ellipse(x, y, 2, 2);
-  canvas2.line(x_margin,y_margin, x_margin,windowHeight-y_margin); //draw y axis
-  canvas2.line(x_margin,windowHeight-y_margin, windowWidth-x_margin,windowHeight-y_margin); //draw x axis
-  
-  
-  canvas2.textSize(32);
-  canvas2.textAlign(CENTER, CENTER);
-  canvas2.text("Time", windowWidth/2, windowHeight-y_margin/2);
-  
-  canvas2.angleMode(DEGREES)
-  //canvas2.rotate(1);
- 
-	canvas2.push()
+	background('white');
+
+
+
+
+
+
+
+
+	if (keyIsDown(UP_ARROW)) {
+		y -= 5;
+	}
+	if (keyIsDown(DOWN_ARROW)) {
+		y += 5;
+	}
+
+	//This section prints the overlay stuff to screen (refresh)
+	textAlign(LEFT, LEFT);
+	fill(255,150)
+	noStroke()
+	textSize(32);
+	fill(0, 102, 153);
+	text("Current Pressure Level :" +y, 10, 30);
+
+
+
+
+	//This part draws the line that has persistence between frames
+	canvas2.stroke(5);
+	canvas2.fill(100);
+	canvas2. ellipse(x, y, 2, 2);
+	canvas2.line(x_margin,y_margin, x_margin,windowHeight-y_margin); //draw y axis
+	canvas2.line(x_margin,windowHeight-y_margin, windowWidth-x_margin,windowHeight-y_margin); //draw x axis
+
+
+	canvas2.textSize(32);
 	canvas2.textAlign(CENTER, CENTER);
-   canvas2.translate(x_margin/2, windowHeight/2)
-   canvas2.rotate(270);
-   canvas2.text("GeeksForGeeks", 0, 0); 
-   //canvas2.rotate(90); 
-   canvas2.textSize(30); 
-  
-  // canvas2.text("GeeksForGeeks", windowHeight/2, -y_margin/2); 
-  //canvas2.rotate(-1);
-   canvas2.pop()
-  
-  
-  // Moving up at a constant speed
-  x = x+dx;
-  
-  
-  
-  // Reset to the bottom
-  if (x > width) {
-    x=0;
-	background(200);
-	
-  }
-  
-  image(canvas2, 0, 0);
-  
-  
+	canvas2.text("Time", windowWidth/2, windowHeight-y_margin/2);
+
+	canvas2.angleMode(DEGREES)
+
+
+	canvas2.push()
+
+	canvas2.textAlign(CENTER, CENTER);
+	canvas2.translate(x_margin/2, windowHeight/2)
+	canvas2.rotate(270);
+	canvas2.text("Pressure", 0, 0); 
+	canvas2.textSize(30); 
+
+	canvas2.pop()
+
+
+	// Moving up at a constant speed
+	x = x+dx;
+
+
+
+	// Reset to the bottom
+	if (x > width) {
+		x=0;
+		background(200);
+
+	}
+
+	image(canvas2, 0, 0);
+
+
 }
 
 
